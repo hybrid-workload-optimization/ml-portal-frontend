016e67f50cc92ee9864a056b679f7b414f800676
ymkwon <ymkwon@strato.co.kr>
2023-03-31 14:46:02 +0900

    ㅂㄱ

--- src/components/SsoLogin.vue
+++ src/components/SsoLogin.vue
@@ -10,7 +10,9 @@ export default {
   },
   methods: {
     async onSubmit() {
-      const redirectUri = `${window.location.protocol}//${window.location.host}/afterSsoLogin`
+      const afterUri = 'afterSsoLogin'
+      const baseUrl = process.env.BASE_URL
+      const redirectUri = `${window.location.protocol}//${window.location.host}${baseUrl}${afterUri}`
       window.location.replace(
         `${process.env.VUE_APP_BASE_API}/gw/login?redirectUrl=${redirectUri}`,
       )
