e8f77def2e681a2dd4d3e49efc321dcd1dca96bb
ymkwon <ymkwon@strato.co.kr>
2023-04-07 13:41:41 +0900

    테스트

--- src/common/request.js
+++ src/common/request.js
@@ -195,9 +195,13 @@ service.interceptors.response.use(
       // cors error -> login 페이지 이동
       // 추후 401 error -> login 페이지 이동으로 변경 예정
     } else if (error.message === 'Network Error') {
+      const originPage = vm.$route.path
       vm.$router
-        .push({ path: '/ssoLogin', query: { originPath: vm.$route.path } })
+        .push({ path: '/logout', query: { originPath: originPage } })
         .catch(() => {})
+      // vm.$router
+      //   .push({ path: '/ssoLogin', query: { originPath: originPage } })
+      //   .catch(() => {})
     } else {
       errorDesc = errorMsg
     }
