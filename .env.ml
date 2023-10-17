cf5d3184376f230616272a706756f6a209a3605b
yeonju <45990362+yjgim@users.noreply.github.com>
2023-10-17 10:05:16 +0900

    서비스 네임 수정

--- .env.ml
+++ .env.ml
@@ -1,7 +1,7 @@
 # base api
 NODE_ENV = ml
 SWAGGER_URL=http://172.16.10.67:18080/v3/api-docs
-VUE_APP_SERVICE_NAME = '/comp-b-svc'
+VUE_APP_SERVICE_NAME = '/interface'
 VUE_APP_BASE_API = 'https://ml.strato.co.kr/interface'
 
 VUE_APP_ENC_KEY = aNdRfUjXn2r5u8x/A?D(G+KbPeShVkYp
@@ -12,4 +12,4 @@ VUE_APP_PUBLIC_CONFIG_URL = 'http://172.16.10.168:9090'
 
 # CoMP FRONT CONFIG 파일 내의 분기처리
 VUE_APP_CONFIG_PUBLIC_PATH = '/comp/'
-VUE_APP_REQUET_BASE_URL = 'https://ml.strato.co.kr/interface/comp-b-svc'
+VUE_APP_REQUET_BASE_URL = 'https://ml.strato.co.kr/interface'
