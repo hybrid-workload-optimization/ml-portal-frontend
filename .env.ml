0aa5d55d7e0da3f198bdc64ea7b28c35a84eb394
yeonju <45990362+yjgim@users.noreply.github.com>
2023-10-19 15:43:33 +0900

    Update .env.ml

--- .env.ml
+++ .env.ml
@@ -5,7 +5,7 @@ VUE_APP_SERVICE_NAME = '/interface'
 VUE_APP_BASE_API = 'https://ml.strato.co.kr/interface'
 
 VUE_APP_ENC_KEY = aNdRfUjXn2r5u8x/A?D(G+KbPeShVkYp
-VUE_APP_AUTOML_API = 'http://172.16.11.244:6006'
+VUE_APP_AUTOML_API = 'https://172.16.11.244:6006'
 
 # public cloud 구성정보 모듈
 VUE_APP_PUBLIC_CONFIG_URL = 'http://172.16.10.168:9090'
