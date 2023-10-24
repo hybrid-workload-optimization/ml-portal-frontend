f1f4afdb93188699772a811e71946788021b4ff5
yeonju <45990362+yjgim@users.noreply.github.com>
2023-10-24 09:12:14 +0900

    #PIM-689 hyperparameter tuning 오류 수정

--- .env.ml
+++ .env.ml
@@ -5,7 +5,8 @@ VUE_APP_SERVICE_NAME = '/interface'
 VUE_APP_BASE_API = 'https://ml.strato.co.kr/interface'
 
 VUE_APP_ENC_KEY = aNdRfUjXn2r5u8x/A?D(G+KbPeShVkYp
-VUE_APP_AUTOML_API = 'https://172.16.11.244:6006'
+# VUE_APP_AUTOML_API = 'https://172.16.11.244:6006'
+VUE_APP_AUTOML_API = 'https://ml.strato.co.kr/ml-b'
 
 # public cloud 구성정보 모듈
 VUE_APP_PUBLIC_CONFIG_URL = 'http://172.16.10.168:9090'
